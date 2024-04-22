143c4c4956bf34b134fc176129e68666ebf7f2e6
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2024-04-22 16:21:57 +0700

    menambahkan member kelas bangunDatar dengan methode input

--- BangunDatar.cpp
+++ BangunDatar.cpp
@@ -4,7 +4,15 @@ using namespace std;
 class bangunDatar{
     //akses modifier
     private:
-        float panjang, lebar;
+        float panjang, Lebar;
     public:
         float luas;
+
+        void input () { // methode input persegi panjang 
+
+            cout << "Masukan Panjangnya = ";
+            cin >> panjang;
+            cout << "Masukan Lebarnya = ";
+            cin >> Lebar;
+        }
 };
